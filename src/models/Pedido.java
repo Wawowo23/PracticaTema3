525af646bfc0728c6bb89bed9e14e852d6b1de9e
wawowo23 <miguelangelcamaracasado@gmail.com>
2024-12-18 17:43:46 +0100

    Metemos los pedidos

--- src/models/Pedido.java
+++ src/models/Pedido.java
@@ -1,6 +1,15 @@
 package models;
 
+import java.time.LocalDate;
+
 public class Pedido {
     private String id;
+    private float precioTotal;
+    private String estado;
+    private String comentario;
+    private Productos producto1;
+    private Productos producto2;
+    private Productos producto3;
+    private LocalDate fechaPedido;
 
 }
